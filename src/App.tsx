a9c0c42d84c762f01105eadbb0d40eb23c041b8f
JulianoAleixo <juliano.moreira.aleixo@gmail.com>
2025-02-07 18:10:26 -0300

    :tada: Init - First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <div className="h-screen flex flex-col">
+            {/* <Navbar /> */}
+            {/* Content */}
+            <div className="flex-1 overflow-y-auto">
+                {/* <Hero /> */}
+                <div className="divider" id="academics" />
+                <div className="divider" id="office" />
+                <div className="divider" id="location" />
+                <div className="divider" />
+                {/* <Footer /> */}
+            </div>
+        </div>
+  )
+}
+
+export default App
